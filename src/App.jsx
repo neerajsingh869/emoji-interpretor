cc129ecce846ad5184e26232b4c124ecbd6946cd
Neeraj Singh <869neerajsingh@gmail.com>
2023-12-04 10:58:32 +0530

    feat: create symbol emojis dictionary

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
+const symbolEmojisDictionary = {
+	"💌": "Love Letter",
+	"💞": "Revolving Hearts",
+	"💥": "Collision",
+	"💢": "Anger Symbol",
+	"💦": "Sweat Droplets",
+	"🤍": "White Heart",
+	"💔": "Broken Heart",
+	"💝": "Heart with Ribbon",
+	"☢️": "Radioactive",
+	"🛜": "Wireless",
+	"🕉️": "Om",
+	"⚕️": "Medical Symbol"
+}
+
 function App() {
 
   return (
